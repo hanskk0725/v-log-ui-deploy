8fa5a1bad6752b97fd5e1ca6cf4c24c492825a1b
hanseong <hans.kk0725@gmail.com>
2026-01-02 20:19:05 +0900

    fix: home

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/posts" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/posts/new" element={<PostCreatePage />} />
